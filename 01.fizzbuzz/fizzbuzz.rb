802171b0a42484fc6d5f2b514de98be1a9367236
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2021-08-06 00:29:11 +0900

    不要なカッコを削除

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,9 +1,9 @@
 (1..20).each do |i|
-  if (i % (3 * 5)) == 0
+  if i % (3 * 5) == 0
     puts "FizzBuzz"
-  elsif (i % 3) == 0
+  elsif i % 3 == 0
     puts "Fizz"
-  elsif (i % 5) == 0
+  elsif i % 5 == 0
     puts "Buzz"
   else
     puts i
