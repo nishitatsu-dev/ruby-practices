fe5b79c13e2e65e442852e8fd651e6ea9033ddeb
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2021-09-10 22:39:47 +0900

    rubocopチェックし、修正。

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -6,20 +6,21 @@ class Score
   end
 
   def count_score(result)
-    pins_result = result.split(",")
-    if pins_result.include?("x")
-      pins_string = pins_result.map { |n| n == "x" ? ["10", "dummy0"] : n }.flatten!
-    else
-      pins_string = pins_result
-    end
-    pins_1to9frame = pins_string[0..17]
-    pins_10frame = pins_string[18..24] - ["dummy0"]
+    results_array = result.split(',')
+    pins = if results_array.include?('X')
+             results_array.map { |n| n == 'X' ? %w[10 dummy0] : n }.flatten!
+           else
+             results_array
+           end
+    pins_1to9frame = pins[0..17]
+    pins_10frame = pins[18..24] - ['dummy0']
     pins_integer = (pins_1to9frame + pins_10frame).map(&:to_i)
+    # 「倒したピン数」に下記①②ボーナスを加算。
+    p pins_integer.sum + bonus_by_spare_or_strike(pins_1to9frame, pins_integer) + bonus_by_strike(pins_1to9frame, pins_integer)
+  end
 
-    # ①倒したピンの合計。
-    base_score = pins_integer.sum
-
-    # ②「スペア又はストライク」のボーナス値。次の投球を加算（＝次のフレームの１投目を加算）。
+  # ①「スペア又はストライク」ボーナス。次の投球を加算（＝次のフレームの１投目を加算）。
+  def bonus_by_spare_or_strike(pins_1to9frame, pins_integer)
     frame_scores = []
     9.times do |n|
       from = 2 * n
@@ -28,25 +29,22 @@ class Score
     end
     frames_of_spare_or_strike = frame_scores.each_index.select { |n| frame_scores[n] == 10 }
     indexes_of_1after = frames_of_spare_or_strike.map { |n| n * 2 + 2 }
-    bonus_by_spare_or_strike = pins_integer.values_at(*indexes_of_1after).sum
+    pins_integer.values_at(*indexes_of_1after).sum
+  end
 
-    # ③「ストライク」のボーナス値。次の次の投球を加算。
-    indexes_of_strike = pins_1to9frame.each_index.select { |n| pins_1to9frame[n] == "dummy0" }
+  # ②「ストライク」ボーナス。次の次の投球を加算。
+  def bonus_by_strike(pins_1to9frame, pins_integer)
+    indexes_of_strike = pins_1to9frame.each_index.select { |n| pins_1to9frame[n] == 'dummy0' }
     indexes_of_2after = indexes_of_strike.map { |n| n + 2 }
-    # ストライクが連続した場合への対応。dummy0の２個後もdummy0になるので、さらに１個後にずらす。
+    # 連続ストライクへの対応。dummy0の次の次がdummy0になるので、さらに１個後にずらす。
     indexes_of_overlap = indexes_of_2after & indexes_of_strike
-    if indexes_of_overlap == [] 
-      bonus_by_strike = pins_integer.values_at(*indexes_of_2after).sum
+    if indexes_of_overlap == []
+      pins_integer.values_at(*indexes_of_2after).sum
     else
       modified_indexes = indexes_of_2after - indexes_of_strike + indexes_of_overlap.map { |n| n + 1 }
-      bonus_by_strike = pins_integer.values_at(*modified_indexes).sum
+      pins_integer.values_at(*modified_indexes).sum
     end
-
-    # 上記①②③の合計
-    p total = base_score + bonus_by_spare_or_strike + bonus_by_strike
   end
 end
 
-if __FILE__ == $0
-  Score.count_score(*ARGV)
-end
+__FILE__ == $0 && Score.count_score(*ARGV)
