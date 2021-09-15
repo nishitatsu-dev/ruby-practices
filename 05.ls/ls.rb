1e358b9263bdb518e72764a3b5715ed38e6ed0ac
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2021-09-15 20:40:33 +0900

    lsコマンド初版
    rubocopはチェック済み

---
+++ 05.ls/ls.rb
@@ -0,0 +1,57 @@
+#! /usr/bin/env ruby
+
+class ListSegment
+  def self.list_segment(path_and_option)
+    self.new.list_segment(path_and_option)
+  end
+
+  def list_segment(path_and_option)
+    options = path_and_option.grep(/-/)
+    paths = path_and_option - options
+
+    if paths == []
+      lists = Dir.glob('*')
+      arrange_form(lists)
+    elsif Dir.exist?(paths[0])
+      Dir.chdir(paths[0])
+      lists = Dir.glob('*')
+      arrange_form(lists)
+    else
+      begin
+        Dir.chdir(paths[0])
+      rescue Errno::ENOENT => e
+        puts e.message.to_s
+      end
+    end
+  end
+
+  def arrange_form(lists)
+    line = 3
+    residue = lists.size % line
+    number_of_dummy = if residue.zero?
+                        0
+                      else
+                        line - residue
+                      end
+    lists_with_dummy = adjust_word_length(lists) + Array.new(number_of_dummy, '')
+    row = lists_with_dummy.size / line
+    lists_2dimension = []
+    line.times do |n|
+      from = row * n
+      to = row * (n + 1) - 1
+      lists_2dimension << lists_with_dummy[from..to]
+    end
+    lists_2d_transposed = lists_2dimension.transpose
+    row.times do |n|
+      puts lists_2d_transposed[n].join
+    end
+  end
+
+  def adjust_word_length(lists)
+    margin = 3
+    word_length = lists.map(&:length).max + margin
+    lists.map { |n| n.ljust(word_length) }
+  end
+end
+
+__FILE__ == $0 && ListSegment.list_segment(ARGV)
