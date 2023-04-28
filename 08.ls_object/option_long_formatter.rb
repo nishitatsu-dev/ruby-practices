d446a5b8efee07d2519e82eaf7a830ded7b6d241
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2023-04-28 15:09:37 +0900

    OptionLongFormatterクラスの修正、その他指摘の修正

--- 08.ls_object/option_long_formatter.rb
+++ 08.ls_object/option_long_formatter.rb
@@ -8,25 +8,35 @@ class OptionLongFormatter
   end
 
   def lines
-    lines = []
-    block = 0
+    @lines = []
+    make_file_lines
+    total_block = @files_with_detail.inject(0) { |result, n| result + n.block }
+    @lines.unshift("total #{total_block}")
+  end
+
+  private
+
+  def make_file_lines
     length_link = calc_max_length(:link)
     length_user = calc_max_length(:user)
     length_group = calc_max_length(:group)
     length_size = calc_max_length(:size)
     @files_with_detail.each do |n|
-      first_half = "#{n.type}#{n.permission} #{n.link.rjust(length_link)} #{n.user.ljust(length_user)}  #{n.group.ljust(length_group)}"
-      second_half = "  #{n.size.rjust(length_size)} #{n.month.rjust(2)} #{n.day.rjust(2)} #{n.time.rjust(5)} #{n.name}"
-      lines << first_half + second_half
-      block += n.block
+      line = n.type.to_s
+      line += "#{n.permission} "
+      line += "#{n.link.rjust(length_link)} "
+      line += "#{n.user.ljust(length_user)}  "
+      line += "#{n.group.ljust(length_group)}  "
+      line += "#{n.size.rjust(length_size)} "
+      line += "#{n.month.rjust(2)} "
+      line += "#{n.day.rjust(2)} "
+      line += "#{n.time.rjust(5)} "
+      line += n.name.to_s
+      @lines << line
     end
-    lines.unshift("total #{block}")
   end
 
-  private
-
   def calc_max_length(item)
-    lengths = @files_with_detail.map { |n| n.method(item).call.length }
-    lengths.max
+    @files_with_detail.map { |n| n.method(item).call.length }.max
   end
 end
